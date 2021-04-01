8f34b14c4f939f40eb914627de3accb36ec7454a
Tony Nguyen <nguyen.tony1015@gmail.com>
2021-04-01 13:17:33 -0400

    end of lesson

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 const app = new Vue({
   el: "#app",
   data: {
-    firstName: "Tony",
-    lastName: "Nguyen",
+    firstName: "",
+    lastName: "",
     email: "",
     ticketQuantity: 1,
     ticketType: "general",
@@ -11,12 +11,27 @@ const app = new Vue({
     purchaseAgreementSigned: false,
   },
   computed: {
-    fullName: function () {
-      if (this.firstName && this.lastName) {
-        return this.firstName + " " + this.lastName;
-      } else {
-        return this.firstName || this.lastName;
-      }
+    fullName: {
+      get: function () {
+        if (this.firstName && this.lastName) {
+          return this.firstName + " " + this.lastName;
+        } else {
+          return this.firstName || this.lastName;
+        }
+      },
+      set: function (newFullName) {
+        const names = newFullName.split(" ");
+
+        if (names.length === 2) {
+          this.firstName = names[0];
+          this.lastName = names[1];
+        }
+
+        if (names.length <= 1) {
+          this.firstName = names[0] || "";
+          this.lastName = "";
+        }
+      },
     },
     ticketDescription: function () {
       let readableTicketType = "General Admission";
@@ -36,6 +51,28 @@ const app = new Vue({
         " " +
         ticketPluralization
       );
+    },
+  },
+  watch: {
+    specialRequests: function (newRequests, oldRequests) {
+      if (
+        newRequests.toLowerCase().includes("meet and greet") ||
+        newRequests.toLowerCase().includes("meet-and-greet")
+      ) {
+        this.ticketType = "vip";
+      }
+    },
+  },
+  methods: {
+    resetFields: function () {
+      this.firstName = "";
+      this.lastName = "";
+      this.email = "";
+      this.ticketQuantity = 1;
+      this.ticketType = "general";
+      this.referrals = [];
+      this.specialRequests = "";
+      this.purchaseAgreementSigned = false;
     },
   },
 });
